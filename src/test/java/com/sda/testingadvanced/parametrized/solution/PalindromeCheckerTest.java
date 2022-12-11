b5dd11c26c90ae3d3f12fcd8522f5c757f207f39
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-12-11 11:36:49 +0100

    -- PalindromeCheckerTest with CsvFileSource

--- src/test/java/com/sda/testingadvanced/parametrized/solution/PalindromeCheckerTest.java
+++ src/test/java/com/sda/testingadvanced/parametrized/solution/PalindromeCheckerTest.java
@@ -3,6 +3,7 @@ package com.sda.testingadvanced.parametrized.solution;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import com.sda.testingadvanced.parametrized.PalindromeChecker;
@@ -12,6 +13,12 @@ class PalindromeCheckerTest {
 	@ParameterizedTest
 	@CsvSource(value = { "kok, true", "sok, false", "kajak, true", "KOK, true", "koK, true", " , false", "\"\", true" })
 	void testedPalindrome(String text, boolean expected) {
+		assertEquals(expected, PalindromeChecker.isPalindrome(text));
+	}
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/palindrome.csv")
+	void testedPalindrome2(String text, boolean expected) {
 		assertEquals(expected, PalindromeChecker.isPalindrome(text));
 	}
 
