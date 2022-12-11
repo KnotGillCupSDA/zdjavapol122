e4aa1c2329bdb81204a47f78b91043619c584508
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-12-11 09:58:07 +0100

    -- calculator test

--- src/test/java/com/sda/testingadvanced/parametrized/CalculatorTest.java
+++ src/test/java/com/sda/testingadvanced/parametrized/CalculatorTest.java
@@ -1,10 +1,19 @@
 package com.sda.testingadvanced.parametrized;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
 
 	@Test
 	void shouldAddTwoPositiveNumbers() {
+		//given
+		Calculator calculator = new Calculator();
+
+		//when
+		double sum = calculator.add(2.0, 3.0);
+
+		//then
+		assertEquals(5.0, sum);
 	}
 }
