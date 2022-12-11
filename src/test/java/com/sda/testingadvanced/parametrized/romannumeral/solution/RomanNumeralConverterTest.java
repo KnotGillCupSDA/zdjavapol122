1bb9074825db8eadbf5fbd142578aa34f3d3efc5
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-12-11 12:39:51 +0100

    -- ArabicToRoman tested with CsvSource and MethodSource

--- src/test/java/com/sda/testingadvanced/parametrized/romannumeral/solution/RomanNumeralConverterTest.java
+++ src/test/java/com/sda/testingadvanced/parametrized/romannumeral/solution/RomanNumeralConverterTest.java
@@ -1,9 +1,15 @@
 package com.sda.testingadvanced.parametrized.romannumeral.solution;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+import java.util.stream.Stream;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import com.sda.testingadvanced.parametrized.romannumeral.ArabicToRoman;
 import com.sda.testingadvanced.parametrized.romannumeral.RomanNumeralConverter;
@@ -17,4 +23,18 @@ class RomanNumeralConverterTest {
 		assertEquals(arabicToRoman.getRoman(), actualRoman);
 	}
 
+	@ParameterizedTest
+	//@CsvSource(value = {"2022, MMXXII", "4587, MMMMDLXXXVII"})
+	@MethodSource("complexNumbers")
+	void testComplexConversions(int arabic, String expectedRoman) {
+		String actualRoman = RomanNumeralConverter.romanFor(arabic);
+		assertEquals(expectedRoman, actualRoman);
+	}
+
+	public static Stream<Arguments> complexNumbers() {
+		return Stream.of(
+				arguments(2022, "MMXXII"),
+				arguments(4587, "MMMMDLXXXVII")
+		);
+	}
 }
